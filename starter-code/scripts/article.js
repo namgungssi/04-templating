dc098b3494f097f010407dc1be49dc4ea1459f2c
dalhun namgung <bnamgung@gmail.com>
2017-04-08 11:21:17 -0700

    finished

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -24,7 +24,7 @@ Article.prototype.toHtml = function() {
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-
+  return article(this);
 };
 
 rawData.sort(function(a,b) {
